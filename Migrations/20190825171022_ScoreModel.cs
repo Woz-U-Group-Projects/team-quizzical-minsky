49707c75139f22b49f60123bf71d14431bf39306
Zack <xbaughman512@gmail.com>
2019-08-25 13:11:33 -0400

    create migration/update database

---
+++ Migrations/20190825171022_ScoreModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NTG.Migrations
+{
+    public partial class ScoreModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
